961e968806d8c4473f16ca2429e9fe21c1fb7aea
PetJs <peterfagoroye76@gmail.com>
2025-01-22 07:41:15 +0100

    Adds the movie card

---
+++ js/renderCard.js
@@ -0,0 +1,34 @@
+// function for fetching the movie from the json file
+const getMovies = async () => {
+    try {
+        const response = await fetch('js/data/movies.json')
+        const data = await response.json() // parsing the json structure 
+        const movies = data.movies
+        renderMovies(movies) // call the renderMovies function
+    }catch(err){
+        console.log(err)
+    }
+}
+
+
+// function for rendering the movies into a card section
+function renderMovies(movieList) {
+    const cardGrid = document.getElementById('card-grid');
+    cardGrid.innerHTML = '';
+
+    movieList.forEach(movie => {
+        const card = document.createElement('div');
+        card.className = 'card';
+        
+        card.innerHTML = `
+            <img src="${movie.image}" alt="${movie.title}">
+            <h3>${movie.title}</h3>
+            <span class="type">${movie.genre}</span>
+        `;
+        
+        cardGrid.appendChild(card);
+    });
+}
+
+// calls the function on every page refresh
+document.addEventListener('DOMContentLoaded', getMovies);
